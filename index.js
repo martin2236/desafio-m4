e20b6e76192294f90e3186f3a3ffd04044807d71
Martin <martoxxx100@gmail.com>
2021-07-21 14:22:21 -0300

    componente header creado

---
+++ index.js
@@ -0,0 +1,5 @@
+function main(){
+const header = document.querySelector(".nav__container")
+crearNav(header)
+}
+main()
---
+++ nav-js/nav.js
@@ -0,0 +1,34 @@
+function crearNav(el){
+    const contenedor = document.createElement("nav")
+    contenedor.classList.add("nav")
+    contenedor.innerHTML = `   
+    <img class="nav__logo" src="./imagenes/Logo.png" alt="Logo" class="header__nav-logo">
+    <div class="nav__menu">
+        <img class="nav__menu-img" src="./imagenes/lista.png" alt="Menu">
+        <a class="nav__menu-link" href="">Portfolio</a>
+        <a class="nav__menu-link" href="">Servicios</a>
+        <a class="nav__menu-link" href="">Contacto</a>
+        <div class="menu-oculto"> 
+            <div class="menu-oculto__display">
+                <img class="menu-oculto__display-close" src="./imagenes/cancel.png" alt="">
+                <a class="menu-oculto__display-a" href="">Portfolio</a>
+                <a class="menu-oculto__display-a" href="">Servicios</a>
+                <a class="menu-oculto__display-a" href="">Contacto</a>
+            </div>
+        </div>
+   </div>
+ </nav>`
+
+ el.appendChild(contenedor)
+  const menuDisplay = document.querySelector(".nav__menu-img")
+ const menu = document.querySelector(".menu-oculto")
+ const cerrarMenu = document.querySelector(".menu-oculto__display-close")
+ menuDisplay.addEventListener("click", (e)=>{
+     e.stopPropagation()
+     menu.style.display = "inherit"
+ })
+ cerrarMenu.addEventListener("click", (e)=>{
+    e.stopPropagation()
+    menu.style.display = ""
+})
+}
