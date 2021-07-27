ab96eb0d6591aa1307e8d919c81ab6a872d6a9b9
Martin <martoxxx100@gmail.com>
2021-07-27 15:09:02 -0300

    casi terminado

--- nav-js/nav.js
+++ nav-js/nav.js
@@ -2,10 +2,11 @@ function crearNav(el){
     const contenedor = document.createElement("nav")
     contenedor.classList.add("nav")
     contenedor.innerHTML = `   
-    <img class="nav__logo" src="./imagenes/Logo.png" alt="Logo" class="header__nav-logo">
+    <a class="" href="./index.html">   <img class="nav__logo" src="./imagenes/Logo.png" alt="Logo" class="header__nav-logo"></a>
+ 
     <div class="nav__menu">
         <img class="nav__menu-img" src="./imagenes/lista.png" alt="Menu">
-        <a class="nav__menu-link" href="./portfolio.html">Portfolio</a>
+        <a id ="nav__menu-link"class="nav__menu-link" href="./portfolio.html">Portfolio</a>
         <a class="nav__menu-link" href="./servicios.html">Servicios</a>
         <a class="nav__menu-link" href="./contacto.html">Contacto</a>
         <div class="menu-oculto"> 
